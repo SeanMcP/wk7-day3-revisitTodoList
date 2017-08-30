c652f4a88819348b5b657b5238f085bc31c71693
Sean McPherson <seanmcp25@gmail.com>
2017-08-30 14:16:03 -0400

    Set up project; add form to top of layout.mustache

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,4 @@
 const express = require('express')
-const mustacheExpress = require('mustache-express')
-const path = require('path')
-const routes = require('./routes/index')
-const morgan = require('morgan')
-
-const app = express()
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.engine('mustache', mustacheExpress())
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'mustache')
-app.set('layout', 'layout')
-
-app.use(routes)
-
-app.use(morgan('dev'))
-
-app.listen(3000, function() {
-  console.log('App is running on localhost:3000');
-})' > server.js
-
-echo 'const express = require('express')
 const router = express.Router()
 
 router.get('/', function(req, res) {
