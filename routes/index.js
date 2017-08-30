283adc1259e0613d706b413b1f09cbd20cf55057
Sean McPherson <seanmcp25@gmail.com>
2017-08-30 14:44:39 -0400

    Add links to todo items

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const models = require('../models/index')
 const router = express.Router()
 
 router.get('/', function(req, res) {
-  res.render('index')
+  models.Todo.findAll()
+  .then(function(data) {
+      console.log('get / data:\n', data)
+      res.render('index', {data: data})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+})
+
+router.post('/create', function(req, res) {
+  models.Todo.create({
+    title: req.body.title
+  })
+  .then(function(data) {
+    console.log('post /create data:\n', data)
+    res.redirect('/')
+  })
+})
+
+router.get('/check/:id', function(req, res) {
+  
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const path = require('path')
-const routes = require('./routes/index')
+const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const routes = require('./routes/index')
 
 const app = express()
-
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.engine('mustache', mustacheExpress())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'mustache')
 app.set('layout', 'layout')
 
-app.use(routes)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(routes)
 app.use(morgan('dev'))
 
 app.listen(3000, function() {
